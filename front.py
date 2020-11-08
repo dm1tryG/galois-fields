14611e37f4b1e51d24b1c35164889ea6a99d7627
dm1tryG <galkin.dmitry.485@gmail.com>
2020-11-08 23:51:05 +0300

    start gui module

---
+++ front.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import (
+    QWidget,
+    QLabel,
+    QLineEdit,
+    QApplication,
+    QPushBu
+)
+
+
+class InputValue(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.lbl = QLabel(self)
+        qle = QLineEdit(self)
+        qle.move(60, 100)
+        self.lbl.move(60, 40)
+        qle.textChanged[str].connect(self.onChanged)
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('SEQUENCE')
+        self.show()
+
+
+    def onChanged(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = InputValue()
+    sys.exit(app.exec_())
